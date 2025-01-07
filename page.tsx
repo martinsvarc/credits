7322c7a51550173690d0b5c5efb72bd92082a663
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2025-01-07 18:32:00 +0100

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,9 @@
+src\app\embed\credit-management\page.tsx
+
+'use client';
+import { CreditManagement } from '@/components/custom/credit-management';
+
+export default function Page() {
+  return <CreditManagement />;
+}
+
